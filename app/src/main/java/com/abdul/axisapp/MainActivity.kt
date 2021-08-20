d6ddd319668743be461a18c4528304a96ac006f5
abdul <abdul.tanveer@gmail.com>
2021-08-20 15:49:42 +0530

    getting the text from edittext and setting its value in textview

--- app/src/main/java/com/abdul/axisapp/MainActivity.kt
+++ app/src/main/java/com/abdul/axisapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.abdul.axisapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var titleEt: EditText
+    lateinit var resultTv: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        titleEt = findViewById(R.id.etTitle)
+        resultTv = findViewById(R.id.tvResult)
+    }
+
+    fun saveDb(view: View) {
+        var dataString = titleEt.text.toString()
+        resultTv.text = dataString
     }
 }
